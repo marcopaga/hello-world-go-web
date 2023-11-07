99df13ae2aeec4a62c0f8c56b6f752e4dd3cd6a3
Marco Paga <marcopaga@users.noreply.github.com>
2023-11-07 09:53:12 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "math/big"
+    "strconv"
+    "time"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, World!")
+}
+
+func calculateFactorial(n int64) *big.Int {
+    if n < 0 {
+        return big.NewInt(0)
+    }
+    
+    result := big.NewInt(1)
+    for i := int64(2); i <= n; i++ {
+        result.Mul(result, big.NewInt(i))
+    }
+    
+    return result
+}
+
+func factorialHandler(w http.ResponseWriter, r *http.Request) {
+    // Get the "number" query parameter from the URL, or use 10 as a default.
+    param := r.URL.Query().Get("number")
+    n, err := strconv.ParseInt(param, 10, 64)
+    if err != nil {
+        n = 100000
+    }
+
+    factorial := calculateFactorial(n)
+    fmt.Fprintf(w, "%d! = %s\n", n, factorial.String())
+}
+
+func main() {
+    http.HandleFunc("/", helloWorldHandler)
+    http.HandleFunc("/factorial", factorialHandler)
+    http.ListenAndServe(":8080", nil)
+}
